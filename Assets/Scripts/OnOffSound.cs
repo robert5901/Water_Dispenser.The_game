de8bb63f13d7fa1586caf12eb7338e8d74376c3a
robert5901 <40694038+robert5901@users.noreply.github.com>
2019-03-24 23:40:37 +0300

    2nd commit
    архив с игрой

---
+++ Assets/Scripts/OnOffSound.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OnOffSound : MonoBehaviour
+{
+    public GameObject sound;
+
+    public void OnMusic()
+    {
+        Debug.Log("s");
+    }
+
+    public void OffMusic()
+    {
+        Debug.Log("ss");
+    }
+}
---
+++ Assets/Scripts/ThrowButtleSong.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ThrowButtleSong : MonoBehaviour
+{
+    public GameObject punchSound;
+    public GameObject ouchSound;
+
+    public IEnumerator OnTriggerEnter2D(Collider2D others)
+    {
+        punchSound.SetActive(true);
+        yield return new WaitForSeconds(1);
+        punchSound.SetActive(false);
+        //yield return new WaitForSeconds(0);
+        ouchSound.SetActive(true);
+        yield return new WaitForSeconds(1);
+        ouchSound.SetActive(false);
+    }
+   
+}
